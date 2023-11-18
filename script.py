f94130322015a6d3c24ec8df3e73ccc0331b2d92
Miguel <miguelmip02@gmail.com>
2023-11-18 13:49:52 -0300

    script melhorado para calcular as medias corretamente

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import subprocess
 import pandas as pd
+import numpy as np
 
 matriz = " matrizes/e40r5000.mtx"
 vetor = " vetores/e40r5000.txt"
@@ -14,23 +15,29 @@ comandos = [
 resultados = {}
 
 for comando in comandos:
-    resultados[comando] = []
+    lista_out = []
     for _ in range(10):
         processo = subprocess.Popen(comando, stdout=subprocess.PIPE, text=True, shell=True)
         saida = processo.communicate()[0]
-        resultados[comando].append(saida)
+        saida = float(saida)
+        lista_out.append(saida)
+    
+    lista_out.remove(min(lista_out))
+    lista_out.remove(max(lista_out))
+    media = np.average(lista_out)
+    resultados[comando] = media
 
 
 lista_resultados = []
 
-for i in range(10):
-    linha_resultado = {"Execução": i+1}
-    for comando, saidas in resultados.items():
-        linha_resultado[comando.split()[0].split('/')[0]] = saidas[i]
-    lista_resultados.append(linha_resultado)
+
+linha_resultado = {}
+for comando, saida in resultados.items():
+    linha_resultado[comando.split()[0].split('/')[0]] = saida
+lista_resultados.append(linha_resultado)
 
 # Criando um DataFrame do pandas com os resultados
 df = pd.DataFrame(lista_resultados)
 
 # Salvar o DataFrame em uma planilha (por exemplo, um arquivo CSV)
-df.to_csv("resultados.csv", index=False)
+df.to_csv("resultados.csv", index=False, sep=';')
